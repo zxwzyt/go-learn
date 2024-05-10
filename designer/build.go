18676e73534a58c5f6453ec41e679af4d2177e42
张晓婉 <zhangxiaowan@qiyi.com>
2024-05-10 18:03:09 +0800

    建造者模式

---
+++ designer/build.go
@@ -0,0 +1,134 @@
+package main
+
+import "fmt"
+
+//产品
+type House struct {
+	windowType string
+	doorType   string
+	floor      int
+}
+
+// ibuilder生成器接口开始
+type IBuilder interface {
+	setWindowType()
+	setDoorType()
+	setNumFloor()
+	getHouse() House
+}
+
+func getBuilder(builderType string) IBuilder {
+	if builderType == "normal" {
+		return newNormalBuilder()
+	}
+
+	if builderType == "igloo" {
+		return newIglooBuilder()
+	}
+	return nil
+}
+
+// NormalBuilder具体生成器
+type NormalBuilder struct {
+	windowType string
+	doorType   string
+	floor      int
+}
+
+func newNormalBuilder() *NormalBuilder {
+	return &NormalBuilder{}
+}
+
+func (b *NormalBuilder) setWindowType() {
+	b.windowType = "Wooden Window"
+}
+
+func (b *NormalBuilder) setDoorType() {
+	b.doorType = "Wooden Door"
+}
+
+func (b *NormalBuilder) setNumFloor() {
+	b.floor = 2
+}
+
+func (b *NormalBuilder) getHouse() House {
+	return House{
+		doorType:   b.doorType,
+		windowType: b.windowType,
+		floor:      b.floor,
+	}
+}
+
+// iglooBuilder具体生成器
+
+type IglooBuilder struct {
+	windowType string
+	doorType   string
+	floor      int
+}
+
+func newIglooBuilder() *IglooBuilder {
+	return &IglooBuilder{}
+}
+
+func (b *IglooBuilder) setWindowType() {
+	b.windowType = "Snow Window"
+}
+
+func (b *IglooBuilder) setDoorType() {
+	b.doorType = "Snow Door"
+}
+
+func (b *IglooBuilder) setNumFloor() {
+	b.floor = 1
+}
+
+func (b *IglooBuilder) getHouse() House {
+	return House{
+		doorType:   b.doorType,
+		windowType: b.windowType,
+		floor:      b.floor,
+	}
+}
+
+// 主管
+type Director struct {
+	builder IBuilder
+}
+
+func newDirector(b IBuilder) *Director {
+	return &Director{
+		builder: b,
+	}
+}
+
+func (d *Director) setBuilder(b IBuilder) {
+	d.builder = b
+}
+
+func (d *Director) buildHouse() House {
+	d.builder.setDoorType()
+	d.builder.setWindowType()
+	d.builder.setNumFloor()
+	return d.builder.getHouse()
+}
+
+func main() {
+	normalBuilder := getBuilder("normal")
+	iglooBuilder := getBuilder("igloo")
+
+	director := newDirector(normalBuilder)
+	normalHouse := director.buildHouse()
+
+	fmt.Printf("normal house door type: %s\n", normalHouse.doorType)
+	fmt.Printf("normal house window type: %s\n", normalHouse.windowType)
+	fmt.Printf("normal house num type: %d\n", normalHouse.floor)
+
+	director.setBuilder(iglooBuilder)
+	iglooHouse := director.buildHouse()
+
+	fmt.Printf("\nIgloo house door type: %s\n", iglooHouse.doorType)
+	fmt.Printf("nIgloo house window type: %s\n", iglooHouse.windowType)
+	fmt.Printf("nIgloo house num type: %d\n", iglooHouse.floor)
+
+}
